26a193e9fb4c8ad3a4dad7c5d95cd9c24faa335e
varun331 <varunnagpal@Varuns-MacBook-Air.local>
2015-10-18 23:51:03 -0400

    shinny apps

---
+++ server.R
@@ -0,0 +1,29 @@
+library(shiny)
+
+# Define server logic for slider examples
+shinyServer(function(input, output) {
+  
+  # Reactive expression to compose a data frame containing all of
+  # the values
+  sliderValues <- reactive({
+    
+    # Compose data frame
+    data.frame(
+      Name = c("V1", 
+               "V2",
+               "V3"),
+      Value = as.character(c(input$integer,
+                             input$integer1,
+                             input$integer2
+                             #paste(input$range, collapse=' '),
+                             #input$format,
+                             #input$animation
+                             )), 
+      stringsAsFactors=FALSE)
+  }) 
+  
+  # Show the values using an HTML table
+  output$values <- renderTable({
+    sliderValues()
+  })
+})
