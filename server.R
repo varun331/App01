35b69d3ce24ae26450ae5dba6920af2a588ff145
varun331 <varunnagpal@Varuns-MacBook-Air.local>
2015-11-11 00:13:49 -0500

    Changes Nov 12th

--- server.R
+++ server.R
@@ -1,20 +1,63 @@
 library(shiny)
 
+
 # Define server logic for slider examples
 shinyServer(function(input, output) {
-  
+  rw <- read.csv("~/Documents/R Projects/McCormick.csv",header=T)
+  rw1 <- rw[,c(8,10,12,14,16,18,21,23,27,29,34,36,38,40,42,44,46,48)]
+  dat <- rw1[,c(1:18)]
+  dat <- na.omit(dat)
+  for (i in 1:18){dat[,c(i)] <- as.numeric(dat[,c(i)])}
+  set.seed(2)
+  sub <- sample(nrow(dat),floor(nrow(dat)*.5))
+  train <- dat[sub,]
+  library(survival)
+  library(lattice)
+  library(parallel)
+  library(gbm)
+  set.seed(1)
+  boost.train=gbm(ER~.,data=train,distribution = "gaussian",
+                  n.trees=5000)  
   # Reactive expression to compose a data frame containing all of
   # the values
   sliderValues <- reactive({
     
     # Compose data frame
     data.frame(
-      Name = c("V1", 
-               "V2",
-               "V3"),
-      Values = as.character(c(input$integer,
+      Name = c("Placement", 
+               "Campaign.Type",
+               "Image.Type",
+               "Receipe.Type",
+               "CTA",
+               "Emotional.or.Functional",
+               "Month.1",
+               "Day.of.Week",
+               "Time.of.Day",
+               "Word.Count",
+               "Question",
+               "Link",
+               "Link.Count",
+               "Blogger.Influencer",
+               "Video",
+               "Content",
+               "Cross.Platform"),
+      Values = as.character   (c(input$integer,
                              input$integer1,
-                             input$integer2
+                             input$integer2,
+                             input$integer3,
+                             input$integer4,
+                             input$integer5,
+                             input$integer6,
+                             input$integer7,
+                             input$integer8,
+                             input$integer9,
+                             input$integer10,
+                             input$integer11,
+                             input$integer12,
+                             input$integer13,
+                             input$integer14,
+                             input$integer15,
+                             input$integer16
                              #paste(input$range, collapse=' '),
                              #input$format,
                              #input$animation
@@ -23,13 +66,54 @@ shinyServer(function(input, output) {
   }) 
   
   # Show the values using an HTML table
-  output$value <- renderPrint({
-   data <- as.numeric(c(input$integer,input$integer1,input$integer2))
-   mean(data)
-      })
+  output$value <-   renderPrint ({ 
+   test1 <- data.frame (c(input$integer,
+                          input$integer1,
+                          input$integer2,
+                          input$integer3,
+                          input$integer4,
+                          input$integer5,
+                          input$integer6,
+                          input$integer7,
+                          input$integer8,
+                          input$integer9,
+                          input$integer10,
+                          input$integer11,
+                          input$integer12,
+                          input$integer13,
+                          input$integer14,
+                          input$integer15,
+                          input$integer16
+                        ))
+   test1 <- t(test1)
+   test1 <- data.frame(test1)
+   rownames(test1) <- c(1)
+   colnames(test1) <-c ("Coded.Placement",
+                        "Coded.Campaign.Type",
+                        "Coded.Image.Type",
+                        "Coded.Recipe.Type",
+                        "Coded.CTA",
+                        "Coded.Emotional.or.Functional",
+                        "Month.1",
+                        "Coded.Day.of.Week",
+                        "Coded.Time.of.Day",
+                        "Coded.Word.Count",
+                        "Coded.Question",
+                        "Coded.Link",
+                        "Coded.Link.Count",
+                        "Coded.Blogger...Influencer",
+                        "Coded.Video",
+                        "Coded.Content",
+                        "Coded.Cross.Platform")
+   yhat.boost <- predict(boost.train,newdata = test1,n.trees = 5000)
+   print(yhat.boost)
+   
+  })
  
   
-})
-change
+  
+  
+  
+})  
 
 
--- ui.R
+++ ui.R
@@ -11,41 +11,85 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       # Simple integer interval
-      sliderInput("integer", "V1:",
-                  min=0, max=1000, value=500),
+      sliderInput("integer", "Placement:",
+                  min=0, max=5, value=50),
       
-      sliderInput("integer1", "V2:",
-                  min=0, max=1000, value=500),
+      sliderInput("integer1", "Campaign.Type:",
+                  min=0, max=7, value=50),
       
-      sliderInput("integer2", "V3:",
-                  min=0, max=1000, value=500)
+      sliderInput("integer2", "Image.Type:",
+                  min=0, max=19, value=50),
       
-      # Decimal interval with step value
-      #sliderInput("decimal", "Decimal:",
-      #            min = 0, max = 1, value = 0.5, step= 0.1),
+      sliderInput("integer3", "Receipe.Type:",
+                  min=2, max=14, value=50),
+     
+       sliderInput("integer4", "CTA:",
+                  min=1, max=16, value=50),
       
+      sliderInput("integer5", "Emotional.or.Functional:",
+                  min=1, max=2, value=500),
       
-      # Specification of range within an interval
-      #sliderInput("range", "Range:",
-      #            min = 1, max = 1000, value = c(200,500)),
+      sliderInput("integer6", "Month.1:",
+                  min=1, max=12, value=50),
+    
+     sliderInput("integer7", "Day.of.Week:",
+                  min=1, max=7, value=500),
+     
+     sliderInput("integer8", "Time.of.Day:",
+                 min=1, max=6, value=500),
+     
+     sliderInput("integer9", "Word.Count:",
+                 min=1, max=5, value=500),
+     
+     sliderInput("integer10", "Question:",
+                 min=1, max=2, value=500),
+     
+     sliderInput("integer11", "Link:",
+                 min=1, max=2, value=500),
+     
+     sliderInput("integer12", "Link.Count:",
+                 min=1, max=3, value=500),
+     
+     sliderInput("integer13", "Blogger.Influencer:",
+                 min=1, max=2, value=500),
+     
+     sliderInput("integer14", "Video:",
+                 min=1, max=2, value=500),
+     
+     sliderInput("integer15", "Content:",
+                 min=1, max=6, value=500), 
+     
+     sliderInput("integer16", "Cross.Platform:",
+                 min=1, max=2, value=500)
+     
       
-      # Provide a custom currency format for value display,
-      # with basic animation
-      #sliderInput("format", "Custom Format:",
-      #            min = 0, max = 10000, value = 0, step = 2500,
-      #            pre = "$", sep = ",", animate=TRUE),
-      
-      # Animation with custom interval (in ms) to control speed,
-      # plus looping
-      #sliderInput("animation", "Looping Animation:", 1, 2000, 1,
-      #           step = 10, animate=
-      #             animationOptions(interval=300, loop=TRUE))
     ),
     
     # Show a table summarizing the values entered
     mainPanel(
-      tableOutput("value")
+    tableOutput("value")
       
     ) 
   )
 ))
+
+# Decimal interval with step value
+#sliderInput("decimal", "Decimal:",
+#            min = 0, max = 1, value = 0.5, step= 0.1),
+
+
+# Specification of range within an interval
+#sliderInput("range", "Range:",
+#            min = 1, max = 1000, value = c(200,500)),
+
+# Provide a custom currency format for value display,
+# with basic animation
+#sliderInput("format", "Custom Format:",
+#            min = 0, max = 10000, value = 0, step = 2500,
+#            pre = "$", sep = ",", animate=TRUE),
+
+# Animation with custom interval (in ms) to control speed,
+# plus looping
+#sliderInput("animation", "Looping Animation:", 1, 2000, 1,
+#           step = 10, animate=
+#             animationOptions(interval=300, loop=TRUE)) 
